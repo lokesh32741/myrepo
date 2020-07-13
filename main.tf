e89403ea84389f9c605555ea007a3033be9cc405
lokesh32741 <66082742+lokesh32741@users.noreply.github.com>
2020-07-12 22:54:14 -0400

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,11 @@
+
+provider "aws" {
+  region = "us-east-1"
+ access_key = "AKIA5I3M6CAR4JYALXAJ"
+  secret_key = "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"
+}
+
+resource "aws_instance" "myec2" {
+  ami= "ami-09d95fab7fff3776c"
+  instance_type = "t2.micro"
+}
